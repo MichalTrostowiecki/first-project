d763132086398d7cf36341ec87127f0c0d2cfac6
MichalTrostowiecki <trostowiecki.michal@gmail.com>
2022-05-02 10:44:07 +0100

    hej dodalem te zmiany

---
+++ 1.js
@@ -0,0 +1,35 @@
+
+let arr = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20];
+
+function toSubArrays (arr){
+    // sort the array numerically
+    let sortArr = arr.sort((a,b) => a-b);
+    // temp holding arrays
+    let tmpArr = [];
+    let result = [];
+    
+    for (let i = 0; i <= sortArr.length; i++) {
+        if ((i === 0) || (sortArr[i] === sortArr[i - 1])){
+        	// add first element or matching element to tmp array
+            tmpArr.push(sortArr[i]);
+            } else {
+            // add tmp array to result array
+            result.push(tmpArr);
+            // clear the tmp array
+            tmpArr = [];
+            // add new value to tmp array
+            tmpArr.push(sortArr[i]);
+        }
+    }
+    return result;
+}
+console.log(toSubArrays(arr));
+Objects.keys(thisi())
+
+const x = "this is my "
+
+const x1 = () => {
+    for (i=0; i<=Array; i++) {
+        
+    }
+}
